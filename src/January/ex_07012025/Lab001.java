6fba409500b9529c45cd099b79db94becee90dd9
pawar <pawaraniket991@gmail.com>
2025-01-09 08:26:25 +0530

    add files

--- src/January/ex_07012025/Lab001.java
+++ src/January/ex_07012025/Lab001.java
@@ -1,4 +1,13 @@
 package January.ex_07012025;
 
 public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO JAVA !");
+        System.out.println("HELLO JAVA !");
+        System.out.println("HELLO JAVA !");
+        System.out.println("HELLO JAVA !");
+        System.out.println("HELLO JAVA !");
+//        System.out.println("");
+    }
 }
